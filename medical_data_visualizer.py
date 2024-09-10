8537b0456bb52bb0cd4e4219db0c364343bf09b1
Md. Sayek Al Sami Piyal <sayekalsami@gmail.com>
2024-09-10 18:29:59 +0000

    3rd

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,54 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
 
-# 2
-df['overweight'] = None
-
-# 3
+df = pd.read_csv('medical_examination.csv')
 
 
-# 4
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
+
+
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
+
+
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
     
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    
+    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
+    
+    
+    fig = sns.catplot(x='variable', hue='value', col='cardio', data=df_cat, kind='count', height=5, aspect=1.2).fig
+    
+    
     fig.savefig('catplot.png')
     return fig
 
 
-# 10
 def draw_heat_map():
-    # 11
-    df_heat = None
-
-    # 12
-    corr = None
-
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
+    
+    
+    corr = df_heat.corr()
+    
+    
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    
+   
+    fig, ax = plt.subplots(figsize=(10, 8))
+    
+    
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', cmap='coolwarm', vmin=-1, vmax=1, center=0, ax=ax)
+    
+   
     fig.savefig('heatmap.png')
     return fig
